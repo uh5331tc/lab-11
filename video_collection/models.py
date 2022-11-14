8129ca5c17c05cedeeabcc541dc35513073ede65
Noelle sdfds <you@gmail.com>
2022-11-14 12:48:43 -0600

    finished video 1

--- video_collection/models.py
+++ video_collection/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+#video table 
+class Video(models.Model):
+    name = models.CharField(max_length=200) #required user input
+    url = models.CharField(max_length=400) #required user input 
+    notes = models.TextField(blank=True, null=True)  #notes feild to be optional in the form 
+
+
+    def __str__(self):
+        return f'ID: {self.pk}, Name: {self.name}, URL: {self.url}, Notes: {self.notes[:200]}'#tuncate to first 200 characters in user notes
--- video_collection/views.py
+++ video_collection/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Video
+from .forms import VideoForm, SearchForm
+from django.contrib import messages 
+from django.core.exceptions import ValidationError
+from django.db import IntegrityError
+from django.db.models.functions import Lower
 
 # Create your views here.
+#create homepage view here
+
+def home(request):
+    app_name = 'Most Watched Videos' #name for my videos 
+    return render(request, 'video_collection/home.html', {'app_name': app_name})
+
+
+def add(request):
+    if request.method == 'POST':   # adding a new video
+        new_video_form = VideoForm(request.POST) 
+        if new_video_form.is_valid():  #is the form valid ???
+            try:
+                new_video_form.save()  # Creates new Video object and saves 
+                return redirect('video_list')
+            except IntegrityError:
+                messages.warning(request, 'You already added that video') #user message
+            except ValidationError:
+                messages.warning(request, 'Invalid YouTube URL')##user message 
+        
+        # Invalid form 
+        messages.warning(request, 'Check the data entered')
+        return render(request, 'video_collection/add.html', {'new_video_form': new_video_form}) 
+            
+        
+    new_video_form = VideoForm()
+    return render(request, 'video_collection/add.html', {'new_video_form': new_video_form}) 
+    
+
+def video_list(request):
+
+    search_form = SearchForm(request.GET)
+
+    if search_form.is_valid():
+        search_term = search_form.cleaned_data['search_term']
+        videos = Video.objects.filter(name__icontains=search_term).order_by(Lower('name'))
+
+    else:
+        search_form = SearchForm()
+        videos = Video.objects.order_by(Lower('name'))
+
+    return render(request, 'video_collection/video_list.html', {'videos': videos, 'search_form': search_form})
+
+
